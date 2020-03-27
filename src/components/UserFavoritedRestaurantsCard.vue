ea2336e6ac68b7dd5d72a98cf7cea908e26d8a66
Miao Yueh Su <bluesmy1020@gmail.com>
2020-03-27 16:54:03 +0800

    Add user page

---
+++ src/components/UserFavoritedRestaurantsCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card mb-3">
+    <div class="card-header">
+      <strong>{{ favoritedRestaurants.length }}</strong> 收藏的餐廳
+    </div>
+    <div class="card-body">
+      <router-link
+        v-for="favoritedRestaurant in favoritedRestaurants"
+        :key="favoritedRestaurant.id"
+        :to="{ name: 'restaurant', params: { id: favoritedRestaurant.id } }"
+      >
+        <img :src="favoritedRestaurant.image" width="60" height="60" class="mr-1 mb-1" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    favoritedRestaurants: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
