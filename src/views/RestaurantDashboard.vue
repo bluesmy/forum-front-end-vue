627095f94c66b060e6328c29ec4978dede50a00c
Miao Yueh Su <bluesmy1020@gmail.com>
2020-04-07 22:19:30 +0800

    Integrate API of restuarants.getRestuarant in RestaurantDashboard

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -15,107 +15,8 @@
 </template>
 
 <script>
-const dummyData = {
-  restaurant: {
-    id: 4,
-    name: "Adalberto Anderson",
-    tel: "313.718.9513 x0422",
-    address: "3047 Marge Key",
-    opening_hours: "08:00",
-    description:
-      " ufun\r\n \r\nQuaerat molestiae placeat fuga et vero neque perferendis qui et. Neque ut a hic dignissimos ea voluptas. Dignissimos distinctio perspiciatis minima.\r\n \r\nRecusandae ut et. Aut ipsum officia voluptatem soluta maiores atque quia. Veniam temporibus occaecati sunt consequuntur tempora dolore. Accusamus qui eum. Possimus dolore et sequi expedita praesentium sed dolore.",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=61.21382535340547",
-    viewCounts: 508,
-    createdAt: "2020-02-28T14:38:32.000Z",
-    updatedAt: "2020-03-26T16:15:15.000Z",
-    CategoryId: 4,
-    Category: {
-      id: 4,
-      name: "墨西哥料理",
-      createdAt: "2020-02-28T14:38:32.000Z",
-      updatedAt: "2020-02-28T14:38:32.000Z"
-    },
-    Comments: [
-      {
-        id: 4,
-        text: "In aut natus qui tempora consequatur.",
-        UserId: 1,
-        RestaurantId: 4,
-        createdAt: "2020-02-28T14:38:32.000Z",
-        updatedAt: "2020-02-28T14:38:32.000Z",
-        User: {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$J9pLpJJ1Tzfe/ZcjdYwXdumyh.3F5E.w/HTxRcH./cl3azhgekgQe",
-          isAdmin: true,
-          image: null,
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-03-02T17:09:40.000Z"
-        }
-      },
-      {
-        id: 54,
-        text: "Harum neque non repellendus eaque id et et aut.",
-        UserId: 2,
-        RestaurantId: 4,
-        createdAt: "2020-02-28T14:38:32.000Z",
-        updatedAt: "2020-02-28T14:38:32.000Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$NyaAtgRuHx3i7hHlnb5IXOC4Uk4.q1J1iQs3op.ymdCEh7.tOwcH2",
-          isAdmin: false,
-          image: null,
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-03-05T12:30:53.000Z"
-        }
-      },
-      {
-        id: 104,
-        text: "Et consequuntur atque officiis beatae.",
-        UserId: 2,
-        RestaurantId: 4,
-        createdAt: "2020-02-28T14:38:32.000Z",
-        updatedAt: "2020-02-28T14:38:32.000Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$NyaAtgRuHx3i7hHlnb5IXOC4Uk4.q1J1iQs3op.ymdCEh7.tOwcH2",
-          isAdmin: false,
-          image: null,
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-03-05T12:30:53.000Z"
-        }
-      },
-      {
-        id: 3662,
-        text: "123",
-        UserId: 1,
-        RestaurantId: 4,
-        createdAt: "2020-03-17T11:57:55.000Z",
-        updatedAt: "2020-03-17T11:57:55.000Z",
-        User: {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$J9pLpJJ1Tzfe/ZcjdYwXdumyh.3F5E.w/HTxRcH./cl3azhgekgQe",
-          isAdmin: true,
-          image: null,
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-03-02T17:09:40.000Z"
-        }
-      }
-    ]
-  }
-};
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
 
 export default {
   name: "RestaurantDashboard",
@@ -134,19 +35,35 @@ export default {
     const { id: restaurantId } = this.$route.params;
     this.fetchRestaurant(restaurantId);
   },
+  beforeRouteUpdate(to, from, next) {
+    const { id: restaurantId } = to.params;
+    this.fetchRestaurant(restaurantId);
+    next();
+  },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurant id: ", restaurantId);
-      const { restaurant } = dummyData;
-      const { id, name, Category, Comments, viewCounts } = restaurant;
-      this.restaurant = {
-        ...this.restaurant,
-        id,
-        name,
-        categoryName: Category ? Category.name : "未分類",
-        commentsLength: Comments.length,
-        viewCounts
-      };
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await restaurantsAPI.getRestaurant(restaurantId);
+
+        if (data.status === "error") {
+          throw new Error(data.message);
+        }
+
+        const { id, name, Category, Comments, viewCounts } = data.restaurant;
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          categoryName: Category ? Category.name : "未分類",
+          commentsLength: Comments.length,
+          viewCounts
+        };
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳資料，請稍後再試"
+        });
+      }
     }
   }
 };
